1de1fdd64f22d47b65a37e64f124dab5d19cd4c3
HGalaroza <hgalaroza@up.edu.ph>
2024-05-08 00:54:13 +0800

    added generate news button after showing facial analysis

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import '../upload.css';
 
-function Modal({ message, onClose, isSuccess }) {
+function Modal({ message, onClose, isSuccess, onViewNews }) {
 
   // Parse the message into rows for table rendering
-  const rows = message.split('\n').map((row, index) => (
-    <tr key={index}>
-      {row.split(':').map((cell, index) => (
-        <td key={index}>{cell}</td>
-      ))}
-    </tr>
-  ));
+  const rows = message.split('\n').map((row, rowIndex) => {
+    const cells = row.split(':');
+    // Check if the row has at least one cell
+    if (cells.length > 1) {
+      return (
+        <tr key={rowIndex}>
+          {cells.map((cell, cellIndex) => (
+            <td key={cellIndex}>{cell}</td>
+          ))}
+        </tr>
+      );
+    } else {
+      return null; // Skip rendering empty rows
+    }
+  });
 
   return (
     <div className={`modal ${isSuccess ? 'success' : 'failure'}`}>
@@ -25,6 +33,11 @@ function Modal({ message, onClose, isSuccess }) {
               {rows}
             </tbody>
           </table>
+        </div>
+        <div className="modal-footer">
+          {isSuccess && (
+            <button onClick={onViewNews}>Generate News</button>
+          )}
         </div>
       </div>
     </div>
